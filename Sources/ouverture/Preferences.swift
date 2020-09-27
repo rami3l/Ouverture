73fc965d572516a1abc8f184f33daa015ffc97c1
rami3l <rami3l@outlook.com>
2020-09-27 17:49:39 +0800

    chore: update formatter settings

--- Sources/ouverture/Preferences.swift
+++ Sources/ouverture/Preferences.swift
@@ -10,23 +10,28 @@ extension Preferences {
             kUTTagClassFilenameExtension,
             ext as CFString,
             nil
-        )?.takeUnretainedValue()
-        Log.verbose("The preferred UTI for extention `\(ext) is `\(res as String? ?? "Unknown")`")
+        )?
+        .takeUnretainedValue()
+        Log.verbose(
+            "The preferred UTI for extention `\(ext) is `\(res as String? ?? "Unknown")`"
+        )
         return res
     }
 
     static func getDefault(for ext: String) -> CFString? {
         guard let uti = getUtiString(for: ext) else { return nil }
-        let res = LSCopyDefaultRoleHandlerForContentType(uti, .all)?.takeUnretainedValue()
-        Log.verbose("The default handler for extention `\(ext) is `\(res as String? ?? "Unknown")`")
+        let res = LSCopyDefaultRoleHandlerForContentType(uti, .all)?
+            .takeUnretainedValue()
+        Log.verbose(
+            "The default handler for extention `\(ext) is `\(res as String? ?? "Unknown")`"
+        )
         return res
     }
 
-    static func setDefault(
-        for ext: String,
-        as cfBundleId: CFString
-    ) -> Bool {
-        Log.verbose("Setting default handler for extention `\(ext) to `\(cfBundleId)`")
+    static func setDefault(for ext: String, as cfBundleId: CFString) -> Bool {
+        Log.verbose(
+            "Setting default handler for extention `\(ext) to `\(cfBundleId)`"
+        )
         guard let uti = getUtiString(for: ext) else { return false }
         let res =
             kOSReturnSuccess
@@ -41,33 +46,26 @@ extension Preferences {
     ) -> [String]? {
         guard #available(macOS 10.11, *) else { return nil }
 
-        Log.verbose("Getting supported file types for \(appDir)")
+        Log.verbose("Getting supported file types for `\(appDir)`")
         let appUrl = URL.init(fileURLWithPath: appDir, isDirectory: true)
         let plistUrl = URL.init(fileURLWithPath: file, relativeTo: appUrl)
 
         let plistAbsPath = plistUrl.absoluteURL.path
         Log.verbose("Reading plist at \(plistAbsPath)")
 
-        guard
-            let plistDict = Plist.readToDict(from: plistAbsPath)
-        else {
+        guard let plistDict = Plist.readToDict(from: plistAbsPath) else {
             Log.error("Unable to read plist at \(plistAbsPath)")
             return nil
         }
 
-        guard
-            let dicts = plistDict["CFBundleDocumentTypes"] as? [[String: Any]]
+        guard let dicts = plistDict["CFBundleDocumentTypes"] as? [[String: Any]]
         else {
             Log.error("Unable to read `CFBundleDocumentTypes` from plist")
             return nil
         }
 
-        var res = [String]()
-        for dict in dicts {
-            if let types = dict["LSItemContentTypes"] as? [String] {
-                res.append(contentsOf: types)
-            }
-        }
+        let res = dicts.map { $0["LSItemContentTypes"] as? [String] ?? [] }
+            .reduce([], +)
 
         Log.verbose("Got supported file types: \n\(res)")
         return res
