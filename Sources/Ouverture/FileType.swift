e4a17e8f76de1fa096718aa5ce6f0eeb58861656
rami3l <rami3l@outlook.com>
2020-10-10 01:57:33 +0800

    refactor: add `FileType` to get DRY

---
+++ Sources/Ouverture/FileType.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+public enum FileType: CustomStringConvertible { case ext(String)
+    case uti(String)
+    case urlScheme(String)
+}
+
+extension FileType {
+    public init(
+        _ fileType: String
+    ) {
+        // * Here we consider that the `fileType`s starting with `.` are extensions, eg. `.txt`,
+        // * those ending with `://` are URL schemes,
+        // * while the reverse domain names are UTIs.
+        // * The fallback case is extension, eg.`txt`.
+        // Good old Rust match hack.
+        switch () {
+        // An extension starting with a dot.
+        case _ where fileType.starts(with: "."):
+            // Truncate the leading dot.
+            self = .ext(String(fileType.dropFirst()))
+            return
+        // An URL Scheme ending with `://`.
+        case _ where fileType.hasSuffix("://"):
+            // Truncate the suffix `://`.
+            self = .urlScheme(String(fileType.prefix(while: { $0 != ":" })))
+            return
+        // A reverse domain is possibly an UTI.
+        case _ where fileType.isReverseDomain():
+            self = .uti(fileType)
+            return
+        default:
+            self = .ext(fileType)
+            return
+        }
+    }
+
+    public var description: String {
+        switch self {
+        case let .ext(x): return ".\(x)"
+        case let .uti(u): return u
+        case let .urlScheme(s): return "\(s)://"
+        }
+    }
+}
--- Sources/ovt/Lookup.swift
+++ Sources/ovt/Lookup.swift
@@ -21,41 +21,24 @@ extension Ovt {
 
         mutating func run() {
             loggerInit(self.options.verbose ? .verbose : .info)
-
-            var hasOutput = false
+            let fileType_ = FileType(fileType)
             print(
-                "`\(fileType)` is claimed by the following:",
+                "`\(fileType_)` is claimed by the following:",
                 terminator: "\n\n"
             )
+            var hasOutput = false
 
-            // * Here we consider that the `fileType`s starting with `.` are extensions, eg. `.txt`,
-            // * those ending with `://` are URL schemes,
-            // * while the reverse domain names are UTIs.
-            // * The fallback case is extension, eg.`txt`.
-            let isExt = fileType.starts(with: ".")
-            let isScm = fileType.hasSuffix("://")
-            let isUti = fileType.isReverseDomain()
             let defaultHandler: CFString? = {
-                /// Good old Rust match hack.
-                switch () {
-                case _ where isExt:
-                    // Truncate the leading dot.
-                    let ext = String(fileType.dropFirst())
+                switch fileType_ {
+                case let .ext(x):
                     return getDefaultHandler(
-                        forExt: ext,
+                        forExt: x,
                         conformingTo: conformingTo as CFString?
                     )
-                case _ where isScm:
-                    // Truncate the suffix `://`.
-                    let scm = fileType.prefix(while: { $0 != ":" })
-                    return getDefaultHandler(forUrlScheme: scm as CFString)
-                case _ where isUti:
-                    return getDefaultHandler(forUti: fileType as CFString)
-                default:
-                    return getDefaultHandler(
-                        forExt: fileType,
-                        conformingTo: conformingTo as CFString?
-                    )
+                case let .urlScheme(s):
+                    return getDefaultHandler(forUrlScheme: s as CFString)
+                case let .uti(u):
+                    return getDefaultHandler(forUti: u as CFString)
                 }
             }()
             defaultHandler.map {
@@ -64,25 +47,16 @@ extension Ovt {
             }
 
             let handlerCandidates: [CFString]? = {
-                switch () {
-                case _ where isExt:
-                    // Truncate the leading dot.
-                    let ext = String(fileType.dropFirst())
+                switch fileType_ {
+                case let .ext(x):
                     return getHandlerCandidates(
-                        forExt: ext,
+                        forExt: x,
                         conformingTo: conformingTo as CFString?
                     )
-                case _ where isScm:
-                    // Truncate the suffix `://`.
-                    let scm = fileType.prefix(while: { $0 != ":" })
-                    return getHandlerCandidates(forUrlScheme: scm as CFString)
-                case _ where isUti:
-                    return getHandlerCandidates(forUti: fileType as CFString)
-                default:
-                    return getHandlerCandidates(
-                        forExt: fileType,
-                        conformingTo: conformingTo as CFString?
-                    )
+                case let .urlScheme(s):
+                    return getHandlerCandidates(forUrlScheme: s as CFString)
+                case let .uti(u):
+                    return getHandlerCandidates(forUti: u as CFString)
                 }
             }()
             handlerCandidates.map {
