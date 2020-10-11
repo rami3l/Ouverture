e7e931ef354ec8f49d6d2da9b8dfa531511ee19e
rami3l <rami3l@outlook.com>
2020-10-11 20:16:17 +0800

    chore: add version string (0.1.0)

--- Sources/ovt/App.swift
+++ Sources/ovt/App.swift
@@ -6,8 +6,11 @@ import LoggerAPI
 import Ouverture
 
 struct Ovt: ParsableCommand {
+    static var version = "0.1.0"
+
     static var configuration = CommandConfiguration(
         abstract: "A default application modifier for macOS.",
+        version: version,
         subcommands: [
             Bind.self, Check.self, Id.self, Lookup.self, Which.self, Uti.self,
         ]
