982f6123adfb131d1f4b0827796757f2cdff213e
rami3l <rami3l@outlook.com>
2020-10-11 21:10:55 +0800

    chore: update Package.swift to be compatible with mint

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Ouverture",
+    products: [
+        .executable(name: "ovt", targets: ["ovt"]),
+        .library(name: "Ouverture", targets: ["Ouverture"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
